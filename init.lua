b5a6be84a260e0ad389281c6fb09fa1d79d180e2
bliybubik-stack <bliybubik@gmail.com>
2026-07-19 21:03:19 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
--- VoRest UIX Entry Point
+-- VoRest UIX v2.0 Entry Point
 local Library = require(script.Library)
 
--- Addon support
 local addons = {
     ThemeManager = pcall(function() return require(script.addons.ThemeManager) end),
     SaveManager = pcall(function() return require(script.addons.SaveManager) end),
+    Animator = pcall(function() return require(script.addons.Animator) end),
+    NotificationManager = pcall(function() return require(script.addons.NotificationManager) end),
 }
 
 return {
     Library = Library,
     ThemeManager = addons.ThemeManager,
     SaveManager = addons.SaveManager,
+    Animator = addons.Animator,
+    NotificationManager = addons.NotificationManager,
 }
